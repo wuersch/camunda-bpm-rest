7e46fce0ad91c955ad5203b9c1e97030b0dc5920
Michael Würsch <wuersch@me.com>
2019-04-11 22:27:47 +0200

    Initial check-in

---
+++ src/main/java/ch/helsana/bpm/CamundaRestApplication.java
@@ -0,0 +1,11 @@
+package ch.helsana.bpm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamundaRestApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CamundaRestApplication.class, args);
+    }
+}
